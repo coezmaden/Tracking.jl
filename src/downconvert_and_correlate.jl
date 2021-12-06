5d0ec05e954f2a916689bb31c60f1f962b82fc99
Can Ozmaden <git@canozmaden.com>
2021-12-06 21:00:04 +0100

    Implement `downconvert_and_correlate`

--- src/downconvert_and_correlate.jl
+++ src/downconvert_and_correlate.jl
@@ -41,7 +41,7 @@ function gen_carrier_replica_kernel!(
 )
     sample_idx   = 1 + ((blockIdx().x - 1) * blockDim().x + (threadIdx().x - 1))
     if sample_idx <= num_samples
-        carrier_replica_im[sample_idx], carrier_replica_re[sample_idx] = CUDA.sincos(2π * ((sample_idx - 1) * carrier_frequency / sampling_frequency + carrier_phase))
+        @inbounds carrier_replica_im[sample_idx], carrier_replica_re[sample_idx] = CUDA.sincos(2π * ((sample_idx - 1) * carrier_frequency / sampling_frequency + carrier_phase))
     end
     return nothing
 end
@@ -61,7 +61,7 @@ function gen_code_replica_kernel!(
     # sample_idx converted to -1:2500 -> [-1 0 +1]
     thread_idx = 1 + ((blockIdx().x - 1) * blockDim().x + (threadIdx().x - 1))
     if thread_idx <= num_samples 
-        code_replica[thread_idx] = codes[1+mod(floor(Int32, code_frequency/sampling_frequency * (thread_idx - num_of_shifts) + start_code_phase), code_length), prn]
+        @inbounds code_replica[thread_idx] = codes[1+mod(floor(Int32, code_frequency/sampling_frequency * (thread_idx - num_of_shifts) + start_code_phase), code_length), prn]
     end
     
     return nothing   
@@ -75,15 +75,10 @@ function downconvert_and_correlate_kernel(
     signal_im,
     carrier_re,
     carrier_im,
-    codes,
-    code_frequency,
+    code,
+    downconverted_signal_re,
+    downconverted_signal_im,
     correlator_sample_shifts,
-    carrier_frequency,
-    sampling_frequency,
-    start_code_phase,
-    carrier_phase,
-    code_length,
-    prn,
     num_samples,
     num_ants,
     num_corrs
@@ -91,34 +86,25 @@ function downconvert_and_correlate_kernel(
     cache = @cuDynamicSharedMem(Float32, (2 * blockDim().x, num_ants, num_corrs))   
     sample_idx   = 1 + ((blockIdx().x - 1) * blockDim().x + (threadIdx().x - 1))
     antenna_idx  = 1 + ((blockIdx().y - 1) * blockDim().y + (threadIdx().y - 1))
-    corr_idx     = 1 + ((blockIdx().z - 1) * blockDim().z + (threadIdx().z - 1))
     iq_offset = blockDim().x
     cache_index = threadIdx().x - 1 
 
-    code_phase = accum_re = accum_im = dw_re = dw_im = carrier_re = carrier_im = 0.0f0
-    mod_floor_code_phase = Int(0)
+    accum_re = accum_im = 0.0f0
 
-    if sample_idx <= num_samples && antenna_idx <= num_ants && corr_idx <= num_corrs
-        # generate carrier
-        carrier_im, carrier_re = CUDA.sincos(2π * ((sample_idx - 1) * carrier_frequency / sampling_frequency + carrier_phase))
-    
+    if sample_idx <= num_samples && antenna_idx <= num_ants
         # downconvert with the conjugate of the carrier
-        dw_re = signal_re[sample_idx, antenna_idx] * carrier_re + signal_im[sample_idx, antenna_idx] * carrier_im
-        dw_im = signal_im[sample_idx, antenna_idx] * carrier_re - signal_re[sample_idx, antenna_idx] * carrier_im
-
-        # calculate the code phase
-        code_phase = code_frequency / sampling_frequency * ((sample_idx - 1) + correlator_sample_shifts[corr_idx]) + start_code_phase
-
-        # wrap the code phase around the code length e.g. phase = 1024 -> modfloorphase = 1
-        mod_floor_code_phase = 1 + mod(floor(Int32, code_phase), code_length)
-
+        downconverted_signal_re[sample_idx] = signal_re[sample_idx] * carrier_re[sample_idx] + signal_im[sample_idx] * carrier_im[sample_idx]
+        downconverted_signal_im[sample_idx] = signal_im[sample_idx] * carrier_re[sample_idx] - signal_re[sample_idx] * carrier_im[sample_idx]
+        
         # multiply elementwise with the code
-        accum_re += codes[mod_floor_code_phase, prn] * dw_re
-        accum_im += codes[mod_floor_code_phase, prn] * dw_im
-    end
+        for (corr_idx, sample_shift) ∈ enumerate(correlator_sample_shifts)
+            accum_re += code[sample_idx - 1 + 2 + sample_shift] * downconverted_signal_re[sample_idx]
+            accum_im += code[sample_idx - 1 + 2 + sample_shift] * downconverted_signal_im[sample_idx]
 
-    cache[1 + cache_index + 0 * iq_offset, antenna_idx, corr_idx] = accum_re
-    cache[1 + cache_index + 1 * iq_offset, antenna_idx, corr_idx] = accum_im
+            cache[1 + cache_index + 0 * iq_offset, antenna_idx, corr_idx] = accum_re
+            cache[1 + cache_index + 1 * iq_offset, antenna_idx, corr_idx] = accum_im
+        end
+    end
 
     ## Reduction
     # wait until all the accumulators have done writing the results to the cache
@@ -127,69 +113,58 @@ function downconvert_and_correlate_kernel(
     i::Int = blockDim().x ÷ 2
     @inbounds while i != 0
         if cache_index < i
-            cache[1 + cache_index + 0 * iq_offset, antenna_idx, corr_idx] += cache[1 + cache_index + 0 * iq_offset + i, antenna_idx, corr_idx]
-            cache[1 + cache_index + 1 * iq_offset, antenna_idx, corr_idx] += cache[1 + cache_index + 1 * iq_offset + i, antenna_idx, corr_idx]
+            cache[1 + cache_index + 0 * iq_offset, antenna_idx] += cache[1 + cache_index + 0 * iq_offset + i, antenna_idx]
+            cache[1 + cache_index + 1 * iq_offset, antenna_idx] += cache[1 + cache_index + 1 * iq_offset + i, antenna_idx]
         end
         sync_threads()
         i ÷= 2
     end
     
     if (threadIdx().x - 1) == 0
-        res_re[blockIdx().x, antenna_idx, corr_idx] += cache[1 + 0 * iq_offset, antenna_idx, corr_idx]
-        res_im[blockIdx().x, antenna_idx, corr_idx] += cache[1 + 1 * iq_offset, antenna_idx, corr_idx]
+        res_re[blockIdx().x, antenna_idx] += cache[1 + 0 * iq_offset, antenna_idx]
+        res_im[blockIdx().x, antenna_idx] += cache[1 + 1 * iq_offset, antenna_idx]
     end
     return nothing
 end
 
 function downconvert_and_correlate_kernel_wrapper(
-    system,
+    res_re,
+    res_im,
     signal,
-    correlator,
-    code_phase,
-    carrier_phase,
-    code_frequency,
+    carrier,
+    code,
+    downconverted_signal,
     correlator_sample_shifts,
-    carrier_frequency,
-    sampling_frequency,
-    signal_start_sample,
-    num_samples_left,
-    prn
+    num_samples,
+    num_ants,
+    num_corrs
 )   
     NVTX.@range "kernel params" begin
-        num_corrs = length(correlator_sample_shifts)
-        num_ants = size(signal, 2)
-        num_samples = size(signal, 1)
-        block_dim_z = num_corrs
+        # keep num_ants in seperate dimensions, truncate num_samples accordingly to fit
         block_dim_y = num_ants
-        # keep num_corrs and num_ants in seperate dimensions, truncate num_samples accordingly to fit
-        block_dim_x = prevpow(2, 1024 ÷ block_dim_y ÷ block_dim_z)
-        threads = (block_dim_x, block_dim_y, block_dim_z)
-        blocks = cld(size(signal, 1), block_dim_x)
-        res_re = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
-        res_im = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
-        shmem_size = sizeof(ComplexF32)*block_dim_x*block_dim_y*block_dim_z
+        block_dim_x = prevpow(2, 1024 ÷ block_dim_y)
+        threads = (block_dim_x, block_dim_y)
+        blocks = cld(num_samples, block_dim_x)
+        shmem_size = sizeof(ComplexF32)*block_dim_x*block_dim_y*num_corrs
     end
     NVTX.@range "downconvert_and_correlate_kernel" begin
         @cuda threads=threads blocks=blocks shmem=shmem_size downconvert_and_correlate_kernel(
-        res_re, 
-        res_im, 
-        signal.re, 
-        signal.im,
-        system.codes,
-        Float32(code_frequency),
-        correlator_sample_shifts,
-        Float32(carrier_frequency),
-        Float32(sampling_frequency),
-        Float32(code_phase),
-        Float32(carrier_phase),
-        size(system.codes, 1),
-        prn,
-        num_samples, 
-        num_ants,
-        num_corrs
+            res_re,
+            res_im,
+            signal.re,
+            signal.im,
+            carrier.re,
+            carrier.im,
+            code,
+            downconverted_signal.re,
+            downconverted_signal.im,
+            correlator_sample_shifts,
+            num_samples,
+            num_ants,
+            num_corrs
     )
     end
     NVTX.@range "sum" begin
-        return sum(res_re .+ 1im*res_im, dims=1)
+        return res_re, res_im
     end
 end
