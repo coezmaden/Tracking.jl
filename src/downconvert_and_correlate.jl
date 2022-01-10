6cf5a0389614573613773efd9db51afdcf8d6192
Can Ozmaden <git@canozmaden.com>
2022-01-10 19:44:59 +0100

    Added kernel wrapper 1

--- src/downconvert_and_correlate.jl
+++ src/downconvert_and_correlate.jl
@@ -219,7 +219,54 @@ function cuda_reduce_partial_sum(
     return CUDA.sum(partial_sum.re, dims=1), CUDA.sum(partial_sum.im, dims=1)
 end
 
-function downconvert_and_correlate_kernel_wrapper(
+function downconvert_and_correlate_kernel_wrapper_1(
+    system,
+    signal,
+    correlator,
+    code_phase,
+    carrier_phase,
+    code_frequency,
+    correlator_sample_shifts,
+    carrier_frequency,
+    sampling_frequency,
+    signal_start_sample,
+    num_samples_left,
+    prn
+)
+    num_corrs = length(correlator_sample_shifts)
+    num_ants = size(signal, 2)
+    num_samples = size(signal, 1)
+    block_dim_z = num_corrs
+    block_dim_y = num_ants
+    # keep num_corrs and num_ants in seperate dimensions, truncate num_samples accordingly to fit
+    block_dim_x = prevpow(2, 1024 ÷ block_dim_y ÷ block_dim_z)
+    threads = (block_dim_x, block_dim_y, block_dim_z)
+    blocks = cld(size(signal, 1), block_dim_x)
+    res_re = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
+    res_im = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
+    shmem_size = sizeof(ComplexF32)*block_dim_x*block_dim_y*block_dim_z
+    @cuda threads=threads blocks=blocks shmem=shmem_size downconvert_and_correlate_kernel(
+        res_re, 
+        res_im, 
+        signal.re, 
+        signal.im,
+        system.codes,
+        Float32(code_frequency),
+        correlator_sample_shifts,
+        Float32(carrier_frequency),
+        Float32(sampling_frequency),
+        Float32(code_phase),
+        Float32(carrier_phase),
+        size(system.codes, 1),
+        prn,
+        num_samples, 
+        num_ants,
+        num_corrs
+    )
+    return sum(res_re .+ 1im*res_im, dims=1)
+end
+
+function downconvert_and_correlate_kernel_wrapper_2(
     res_re::CuArray,
     res_im::CuArray,
     signal::StructArray,
